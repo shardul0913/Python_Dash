7582639d5ef0048ba8e38e5c63d178028224e0c0
shardul0913 <shardul.rane0913@gmail.com>
2020-04-03 00:10:22 -0400

    commit second

--- Index.py
+++ Index.py
@@ -1,3 +1,18 @@
-import plotly as plt
-print("yolo")
+import dash_core_components as dcc
+import dash_html_components as html
+import dash
+from dash.dependencies import Input, Output
+import flask
+
+app = dash.Dash()
+app.layout = html.Div(children=[
+                dcc.Input(id='input',value='Enter Text',type='text'),
+                html.Div(id='output')
+               ])
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
+
+
 
